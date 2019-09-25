84e13b8d9f56e25d63043121279697f4d332624e
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-09-25 23:15:16 +0900

    fix

--- push.sh
+++ push.sh
@@ -8,6 +8,9 @@ if [[ $? -eq 1 ]]; then
   git add -A
   git commit -m "generate [ci skip]"
   git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${TRAVIS_BRANCH}
+  git add -A
+  git commit -m "generate [ci skip]" -v
+  git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${TRAVIS_BRANCH}
 else
   echo 'There are no changes'
 fi
