28307b4d203d93f54d8314e6fccfa2ca9ffc42ff
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-09-25 22:59:00 +0900

    fix

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+git add -N .
+set +e
+git diff --exit-code --quiet
+if [[ $? -eq 1 ]]; then
+  git add -A
+  git commit -m "generate [ci skip]"
+  git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${TRAVIS_BRANCH}
+else
+  echo 'There are no changes'
+fi
