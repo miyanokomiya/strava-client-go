497e8d9db41b308812885b26794e44c95dc57bf9
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-09-26 01:19:26 +0900

    save spec.json

--- Makefile
+++
@@ -1,2 +0,0 @@
-generate:
-	docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i https://developers.strava.com/swagger/swagger.json -g go -o /local -c /local/config.json --skip-validate-spec
--- generate.sh
+++ generate.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/bash
 
-VERSION=`ruby ./migrate.rb`
-echo generate:${VERSION}
-
 git checkout master
 git pull origin master
-make generate
-git checkout .travis.yml
-
-set -e
-git add -N .
-set +e
-git diff --exit-code --quiet
+curl https://developers.strava.com/swagger/swagger.json > spec.json
+git diff spec.json --exit-code --quiet
 if [[ $? -eq 1 ]]; then
+  VERSION=`ruby ./migrate.rb`
+  echo generate:${VERSION}
+  docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/spec.json -g go -o /local -c /local/config.json --skip-validate-spec
+  git checkout .travis.yml
   git add -A
-  git commit -m "generate [ci skip]"
+  git commit -m "generate:${VERSION} [ci skip]"
   git tag -a ${VERSION} -m ${VERSION}
   git push --quiet https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master
   git push --quiet https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git --tags
