58803f4351db5ef8f93a10bbd783bfe73be79d80
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-09-25 23:29:42 +0900

    fix

--- push.sh
+++ push.sh
@@ -5,10 +5,10 @@ git add -N .
 set +e
 git diff --exit-code --quiet
 if [[ $? -eq 1 ]]; then
-  git checkout master
+  git checkout -b generate
   git add -A
   git commit -m "generate [ci skip]"
-  git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${TRAVIS_BRANCH}
+  git push --quiet https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${TRAVIS_BRANCH}
 else
   echo 'There are no changes'
 fi
